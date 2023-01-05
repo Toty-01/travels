118dca88b57b8cb36ba0bd65c38115d8ab3459fc
Toty-01 <caucheanthony@gmail.com>
2023-01-05 14:37:25 +0100

    middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js'
 
@@ -13,23 +13,31 @@ export const signin = async (req, res) => {
         if(!isPasswordCorrect) return res.status(404).json({ message: "Invalid credentials" });
 
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1"});
+       
+        res.status(200).json({ result: existingUser, token });
+
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        res.status(500).json({ message: "Something is wrong" });
     }
 }
 
 export const signup = async (req, res) => { 
-    const { id } = req.params;
+    const { email, password, confirmPassword, firstName, lastName } = req.body;
 
     try {
-        const post = await PostMessage.findById(id);
+        const existingUser = await User.findOne({ email });
+        if(existingUser) return res.status(400).json({ message: "User already exist" });
+        if(password !== confirmPassword) return res.status(400).json({ message: "Password don t match" });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
+
+        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" }); 
+
+        res.status(200).json({ result: result, token });
         
-        res.status(200).json(post);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        res.status(500).json({ message: 'Something is wrong' });
     }
 }
-
-
-
-export default router;
